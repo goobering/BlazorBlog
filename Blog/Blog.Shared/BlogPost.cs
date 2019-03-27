d76182652f297d63d225d9140db0da6211db235c
Dale Johnston <goobering@gmail.com>
2019-03-27 16:06:34 +0000

    Created BlogPost class

---
+++ Blog/Blog.Shared/BlogPost.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace PersonalBlog.Blog.Shared
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Post { get; set; }
+        public string PostSummary
+        {
+            get
+            {
+                if(Post.Length < 50)
+                    return Post;
+                else
+                    return Post.Substring(0, 50);
+            }
+        }
+        public DateTime Posted { get; set; }
+
+    }
+}
